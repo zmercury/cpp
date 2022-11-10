6d898252f6f4c0f54fb8b32b0997ec482b88e2c0
Bastola <messiniki@outlook.com>
2022-11-10 07:36:41 +0545

    Nov 10

--- Notes/003_Nov10/main.cpp
+++ Notes/003_Nov10/main.cpp
@@ -3,20 +3,7 @@
 using namespace std;
 
 int main() {
-    double simpleInterest, principal, rate, time;
 
-    cout << "Enter the Principal: " << endl;
-    cin >> principal;
-
-    cout << "Enter the Rate: " << endl;
-    cin >> rate;
-    
-    cout << "Enter the Time: " << endl;
-    cin >> time;
-
-    simpleInterest = (principal * time * rate) / 100;
-
-    cout << "The simple interest is " << simpleInterest;
 
     return 0;
 }
