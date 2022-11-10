a652282545e1e8b40d7b9684898c852d83d71b85
Bastola <messiniki@outlook.com>
2022-11-10 07:44:00 +0545

    Nov 10

--- Notes/003_Nov10/main.cpp
+++ Notes/003_Nov10/main.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
+    float areaOfCircle, radius;
+    double pi = 3.14;
 
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    areaOfCircle = pi * radius * radius;
+
+    cout << "The area of the circle is " << areaOfCircle << " cm2";
 
     return 0;
 }
